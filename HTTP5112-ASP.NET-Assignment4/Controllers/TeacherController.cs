c111e6f0a29b20532f05bf474f815a5153778aab
xRayMonDxp <raymond980711@gmail.com>
2022-11-28 10:56:54 -0500

    first commit

---
+++ HTTP5112-ASP.NET-Assignment4/Controllers/TeacherController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using HTTP5112Assignment4.Models;
+using System.Diagnostics;
+
+namespace HTTP5112Assignment4.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET : /Teacher/List
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.ListTeacher(SearchKey);
+            ViewBag.SearchKey = SearchKey;
+            return View(Teachers);
+        }
+
+        //GET : /Tecaher/Show/{id}
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+            IEnumerable<Class> Classes = controller.ListClasses(id);
+            ViewBag.classes = Classes;
+
+            return View(NewTeacher);
+        }
+
+        //GET : /Teacher/DeleteConfirm/{id}
+        public ActionResult DeleteConfirm(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+
+            return View(NewTeacher);
+        }
+
+
+        //POST : /Teacher/Delete/{id}
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            controller.DeleteTeacher(id);
+
+            return RedirectToAction("List");
+        }
+
+        //GET : /Teacher/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        //GET : /Teacher/Ajax_New
+        public ActionResult Ajax_New()
+        {
+            return View();
+        }
+
+        //POST : /Teacher/Create
+        [HttpPost]
+        public ActionResult Create(string TeacherFname, string TeacherLname, string EmployeeNumber, string HireDate, string Salary)
+        {
+            //Identify that this method is running
+            //Identify the inputs provided from the form
+             
+            Debug.WriteLine("I have accessed the Create Method!");
+            Debug.WriteLine(TeacherFname);
+            Debug.WriteLine(TeacherLname);
+            Debug.WriteLine(Salary);
+
+            Teacher NewTeacher = new Teacher();
+            NewTeacher.TeacherFname = TeacherFname;
+            NewTeacher.TeacherLname = TeacherLname;
+            NewTeacher.EmployeeNumber = EmployeeNumber;
+            NewTeacher.HireDate = HireDate;
+            DateTime HireDateDT;
+            if (!DateTime.TryParse(HireDate, out HireDateDT))
+            {
+                NewTeacher.HireDate = null;
+            }
+            if (!float.TryParse(Salary, out NewTeacher.Salary))
+            {
+                NewTeacher.Salary = float.NaN;
+            }
+
+            if (NewTeacher.TeacherFname == null || NewTeacher.TeacherLname == null || NewTeacher.EmployeeNumber == null || NewTeacher.HireDate == null || Salary == null)
+            {
+                return View(NewTeacher);
+            }
+            TeacherDataController controller = new TeacherDataController();
+            controller.AddTeacher(NewTeacher);
+
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+﻿# Humber College - Web Development Program - HTTP5112 - ASP.NET - Assignment 4
+
+This repo connects to the school database which contains tables for teachers, students, classe, etc. The [Controller](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/tree/master/HTTP5112-ASP.NET-Assignment4/Controllers) folder contains different controller files to retrieve information from the database.
+
+## Minimum Viable Product (MVP)
+
+### User can view list of teachers
+
+**GET** `http://localhost/Teacher/List`<br/>
+Returns a list of teachers. <br/>
+This is handled in the [TeacherController.cs](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/blob/master/HTTP5112-ASP.NET-Assignment4/Controllers/TeacherController.cs).<br/>
+Please refer to the documentation in the file for more details.
+
+### User can view a specific teacher based on the teacher id
+
+**GET** `http://localhost/Teacher/Show/{id}`<br/>
+Returns a specifc teacher based on {id} which represents the teacher id.<br/>
+This is handled in the [TeacherController.cs](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/blob/master/HTTP5112-ASP.NET-Assignment4/Controllers/TeacherController.cs).<br/>
+Please refer to the documentation in the file for more details.
+
+### User can search for teachers in the search bar
+
+![Highlighting the search bar location in the webpage](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/blob/master/HTTP5112-ASP.NET-Assignment4/Readme%20Images/Search%20bar.png)
+
+### User can view list of classes
+
+**GET** `http://localhost/Class/List`<br/>
+Returns a list of classes. <br/>
+This is handled in the [ClassController.cs](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/blob/master/HTTP5112-ASP.NET-Assignment4/Controllers/ClassController.cs).<br/>
+Please refer to the documentation in the file for more details.
+
+### User can view a specific class based on the class id
+
+**GET** `http://localhost/Class/Show/{id}`<br/>
+Returns a specifc class based on {id} which represents the class id.<br/>
+This is handled in the [ClassController.cs](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/blob/master/HTTP5112-ASP.NET-Assignment4/Controllers/ClassController.cs).<br/>
+Please refer to the documentation in the file for more details.
+
+### User can view list of students
+
+**GET** `http://localhost/Student/List`<br/>
+Returns a list of students. <br/>
+This is handled in the [StudentController.cs](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/blob/master/HTTP5112-ASP.NET-Assignment4/Controllers/StudentController.cs).<br/>
+Please refer to the documentation in the file for more details.
+
+### User can view a specific student based on the student id
+
+**GET** `http://localhost/Student/Show/{id}`<br/>
+Returns a specifc student based on {id} which represents the student id.<br/>
+This is handled in the [StudentController.cs](https://github.com/raymondleemv/HTTP5112-ASP.NET-Assignment4/blob/master/HTTP5112-ASP.NET-Assignment4/Controllers/StudentController.cs).<br/>
+Please refer to the documentation in the file for more details.
